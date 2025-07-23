8eadc93ae5c1a281668fc5e3e9a7bd61b450d894
Takauka <k22m2022@cis.twcu.ac.jp>
2025-07-24 07:56:48 +0900

    変更

--- test.py
+++ test.py
@@ -183,7 +183,7 @@ for feta in range(len(paths)):
         model = social_stgcnn(n_stgcnn =args.n_stgcnn,n_txpcnn=args.n_txpcnn,
         output_feat=args.output_size,seq_len=args.obs_seq_len,
         kernel_size=args.kernel_size,pred_seq_len=args.pred_seq_len).to(device)
-        model.load_state_dict(torch.load(model_path))
+        model.load_state_dict(torch.load(model_path, map_location=device))
 
 
         ade_ =999999
